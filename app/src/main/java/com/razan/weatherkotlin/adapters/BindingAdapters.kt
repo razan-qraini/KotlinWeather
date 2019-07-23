a8d6d9e1a7421f0b6f127f59b2306956479d650a
Razan Qraini <razan.qraini@harri.com>
2019-07-23 18:40:30 +0300

    Fixed string concatenation

--- app/src/main/java/com/razan/weatherkotlin/adapters/BindingAdapters.kt
+++ app/src/main/java/com/razan/weatherkotlin/adapters/BindingAdapters.kt
@@ -12,7 +12,6 @@ object BindingAdapters{
     @BindingAdapter("imageUrl")
     @JvmStatic
     fun setImageUrl(view: ImageView, url: String) {
-
-        Picasso.get().load(FLAG_URL + url + ".png").fit().into(view) // TODO GLIDE
+        Picasso.get().load("$FLAG_URL$url.png").fit().into(view)
     }
 }
